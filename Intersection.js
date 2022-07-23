1a19ab425f4b1fedbfd06372ab7f932ffe5f5583
yenting <yenting@yentingdeMacBook-Pro.local>
2022-07-24 01:59:56 +0800

    fix: deleting  repeat numbers in input array

--- Intersection.js
+++ Intersection.js
@@ -5,11 +5,23 @@
 // intersection([1,2,3,4,5,6],[5,6,7])
 // Answer is [5,6]
 
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [9,4]
+// Explanation: [4,9] is also accepted.
+
 const arr1 = [1, 2, 3, 4, 5, 6];
 const arr2 = [5, 6, 7, 7, 8];
 
 const intersection = (arr1, arr2) => {
-  let arr3 = arr1.concat(arr2);
+  let arr1_uq = [...new Set(arr1)];
+  let arr2_uq = [...new Set(arr2)];
+  let arr3 = arr1_uq.concat(arr2_uq);
   //console.table(arr3);
   let counter = {};
   let answer = [];
