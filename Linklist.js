15652afb976ebb5c9b221f88c9bb2d61e2708ca4
yenting <yenting@yentingdeMacBook-Pro.local>
2022-09-04 14:00:08 +0800

    feat: Linklist-pop
    add pop property method

--- Linklist.js
+++ Linklist.js
@@ -16,7 +16,9 @@ class Linklist {
     // this : linklist
     if (this.length === 0) {
       this.head = newNode;
-    } else {
+    }
+    //  case 2 : 已有節點，現在加入新節點
+    else {
       let currentNode = this.head;
       while (currentNode.next !== null) {
         currentNode = currentNode.next;
@@ -25,6 +27,29 @@ class Linklist {
     }
     this.length++;
   } // push
+  // pop 掉最後一個節點
+  pop() {
+    // 沒有任何一個節點
+    if (!this.head) {
+      return;
+    }
+    // 只有一個節點
+    else if (this.length === 1) {
+      let temp = this.head;
+      this.head = null;
+      this.length = 0;
+      return temp;
+    } else {
+      let currentNode = this.head;
+      for (let i = 1; i <= this.length - 2; i++) {
+        currentNode = currentNode.next;
+      }
+      let temp = currentNode.next;
+      currentNode.next = null;
+      this.length--;
+      return temp;
+    }
+  } // pop
   printAll() {
     if (this.length == 0) {
       console.log(`nothing in the linklist`);
@@ -43,4 +68,5 @@ myLinklist.push("Tim");
 myLinklist.push("Mike");
 myLinklist.push("Jame");
 myLinklist.push("Jason");
+console.log(myLinklist.pop());
 myLinklist.printAll();
