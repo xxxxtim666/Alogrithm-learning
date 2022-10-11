a43a37622867dafdf9eb037bce887f533a966653
yenting <yenting@yentingdeMacBook-Pro.local>
2022-10-11 23:07:09 +0800

    feat: add parse function
    parse key to ascii number if key is not number

--- HashTable.js
+++ HashTable.js
@@ -14,8 +14,18 @@ class Hashtable {
 
   // multiplication method
   hash_2(key) {
+    let parsedkey = typeof key !== "number" ? this.parse(key) : key;
     const A = (Math.sqrt(5) - 1) / 2;
-    return Math.floor(this.size * ((key * A) % 1));
+    return Math.floor(this.size * ((parsedkey * A) % 1));
+  }
+
+  // parse string to number
+  parse(str) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+      result += str.charCodeAt(i);
+    }
+    return result % this.size;
   }
   set(key, value) {
     // value: Mike key: 11545
@@ -37,10 +47,15 @@ class Hashtable {
 }
 
 let myHashTable = new Hashtable(6);
-myHashTable.set(11424, "mike");
-myHashTable.set(14, "tim");
-myHashTable.set(113, "Jason");
+// myHashTable.set(11424, "mike");
+// myHashTable.set(14, "tim");
+// myHashTable.set(113, "Jason");
+
+// css
+myHashTable.set("white", "#FFFFFF");
+myHashTable.set("magenta", "#FF00FF");
+myHashTable.set("red", "#FF0000");
 
 myHashTable.printAll();
 
-console.log(myHashTable.get(113));
+console.log(myHashTable.get("red"));
